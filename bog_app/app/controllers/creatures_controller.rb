814602daa180fff60fae1eac3040958207c751f5
Matt Sullivan <mattbsullivan@gmail.com>
2016-06-15 12:26:59 -0700

    BOGGY BOGSTEIN

---
+++ bog_app/app/controllers/creatures_controller.rb
@@ -0,0 +1,44 @@
+class CreaturesController < ApplicationController
+  def index
+    @creatures = Creature.all
+    render :index
+  end
+
+  def new
+    @creature = Creature.new
+    render :new
+  end
+  def create
+    creature_params = params.require(:creature).permit(:name, :description)
+    creature = Creature.create(creature_params)
+    redirect_to creatures_path(creature)
+
+  end
+
+  def show
+    creature_id = params[:id]
+    @creature = Creature.find_by_id(creature_id)
+    render :show
+  end
+
+  def edit
+    creature_id = params[:id]
+    @creature = Creature.find_by_id(creature_id)
+    render :edit
+  end
+
+  def update
+    creature_id = params[:id]
+    creature = Creature.find_by_id(creature_id)
+    creature_params = params.require(:creature).permit(:name, :description)
+    creature.update_attributes(creature_params)
+    redirect_to creature_path(creature)
+  end
+
+  def destroy
+    creature_id = params[:id]
+    creature = Creature.find_by_id(creature_id)
+    creature.destroy
+    redirect_to creatures_path
+  end
+end
---
+++ bog_app/db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Creature.create({name: "Gollum", description: "originally known as Sméagol, was at first a Stoor, one of the three early Hobbit-types. Deformed and twisted in body and mind by the corruption of the Ring, his chief desire was to possess the very Ring that had enslaved him."})
+Creature.create({name: "Swamp Thing", description: "A humanoid mass of vegetable matter who fights to protect his swamp home, the environment in general, and humanity from various supernatural or terrorist threats."})
